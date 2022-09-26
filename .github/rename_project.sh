3a06ee4cc21d3984044b5090e5d7cec503da0885
san-soucie <san-soucie@users.noreply.github.com>
2022-09-26 16:18:23 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="san-soucie"
+original_name="gvip"
+original_urlname="gvip"
+original_description="Awesome gvip created by san-soucie"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv gvip $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
